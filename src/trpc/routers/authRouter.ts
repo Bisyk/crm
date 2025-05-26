cbaea1251a3ab1b8bbd7acd2020dca65cdede88d
Bisyk <99905134+Bisyk@users.noreply.github.com>
2025-05-26 22:37:51 +0300

    feat: refactor signup and login procedures to use zod schemas for validation

--- src/trpc/routers/authRouter.ts
+++ src/trpc/routers/authRouter.ts
@@ -1,34 +1,24 @@
 import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "../init";
 import * as authService from "../services/auth";
+import { signupSchema } from "@/schemas/signup.schema";
+import { loginSchema } from "@/schemas/login.schema";
 
 export const authRouter = createTRPCRouter({
-  signup: baseProcedure
-    .input(
-      z.object({
-        email: z.string().email(),
-        password: z.string().min(8),
-        firstName: z.string().min(2),
-        lastName: z.string().min(2),
-        shopName: z.string().min(2),
-      })
-    )
-    .mutation(async opts => {
-      const res = await authService.signup(opts.input);
-      if (!res.success) {
-        return { success: false, error: res.error };
-      }
-      return { success: true };
-    }),
-  login: baseProcedure
-    .input(z.object({ email: z.string().email(), password: z.string().min(8) }))
-    .mutation(async opts => {
-      const res = await authService.login(opts.input);
-      if (!res.success) {
-        return { success: false, error: res.error };
-      }
-      return { success: true };
-    }),
+  signup: baseProcedure.input(signupSchema).mutation(async opts => {
+    const res = await authService.signup(opts.input);
+    if (!res.success) {
+      return { success: false, error: res.error };
+    }
+    return { success: true };
+  }),
+  login: baseProcedure.input(loginSchema).mutation(async opts => {
+    const res = await authService.login(opts.input);
+    if (!res.success) {
+      return { success: false, error: res.error };
+    }
+    return { success: true };
+  }),
   logout: baseProcedure.mutation(async () => {
     await authService.logout();
   }),
